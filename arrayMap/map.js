de0edc862bb7f7a3fb2c8cf8ff6b881d97dbf84b
Jubayer husain <jubayerhusai360@gmail.com>
2024-09-27 21:35:49 +0600

    first index get

--- arrayMap/map.js
+++ arrayMap/map.js
@@ -1,7 +1,14 @@
-const number  = [12, 21,43,13,74];
-const mapNumber1 = number.map(n => n*2);
-const mapNumber2 = number.map(n => n/2);
-console.log(mapNumber1);
-console.log(mapNumber2);
-const sum = mapNumber1+mapNumber2
-console.log(sum);
+// const number  = [12, 21,43,13,74];
+// const mapNumber1 = number.map(n => n*2);
+// const mapNumber2 = number.map(n => n/2);
+// console.log(mapNumber1);
+// console.log(mapNumber2);
+// console.log(sum);
+
+// const name = ['Rifat', 'Sifat', 'Jihad', 'Junaed'];
+// const nameMap = name.map(n =>n.length);
+// console.log(nameMap);
+
+const name = ['Rifat', 'Sifat', 'Jihad', 'Junaed'];
+const firstIndex = name.map(n => n[0]);
+console.log(firstIndex);
