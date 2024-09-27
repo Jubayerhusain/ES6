8836ab39564d0f74e803f1fadd235efb2d83604d
Jubayer husain <jubayerhusai360@gmail.com>
2024-09-27 16:47:31 +0600

    map.js

---
+++ arrayMap/map.js
@@ -0,0 +1,7 @@
+const number  = [12, 21,43,13,74];
+const mapNumber1 = number.map(n => n*2);
+const mapNumber2 = number.map(n => n/2);
+console.log(mapNumber1);
+console.log(mapNumber2);
+const sum = mapNumber1+mapNumber2
+console.log(sum);
