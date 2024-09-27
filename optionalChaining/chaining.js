7e58224e7a9c2437ea8193a2fbfbf0b7490e769e
Jubayer husain <jubayerhusai360@gmail.com>
2024-09-27 16:35:00 +0600

    map.js amd chaining.js

---
+++ optionalChaining/chaining.js
@@ -0,0 +1,13 @@
+const user = {
+    id: 5004,
+    name: 'Rifat',
+    address:{
+        upojela: 'Aditmary',
+        jela: 'Lalmonirhat',
+        division: 'Rangpur',
+        country: 'Bangladesh'
+    }
+}
+const details = user;
+console.log(details);
+// console.log(user.address.jela.union?.gram); 
